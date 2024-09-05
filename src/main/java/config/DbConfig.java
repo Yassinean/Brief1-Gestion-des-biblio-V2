70582787002908674e0af18255d4881f904f2b28
Yassinean <yassinedubraska@gmail.com>
2024-09-05 21:21:16 +0100

    Adding DAO Layer

---
+++ src/main/java/config/DbConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConfig {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null) {
+            try {
+                Class.forName("org.postgresql.Driver");
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "hanach");
+                System.out.println("Database connection established successfully!");
+            } catch (ClassNotFoundException | SQLException e) {
+                System.out.println("Failed to connect to the database.");
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import config.DbConfig;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Connection connection = DbConfig.getConnection();
+            if (connection != null) {
+                System.out.println("Connected to the database successfully!");
+            } else {
+                System.out.println("Failed to connect to the database.");
+            }
+        } catch (Exception e) {
+            System.out.println("Exception occurred while trying to connect to the database.");
+            e.printStackTrace();
+        }
     }
 }
