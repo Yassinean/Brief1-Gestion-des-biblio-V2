fc1a07d5a92e726140fe85fdadc71f03ca604fcc
Yassinean <yassinedubraska@gmail.com>
2024-09-14 19:16:26 +0100

    adding empruntable interface

--- src/main/java/org/yassine/metier/Interface/Empruntable.java
+++ src/main/java/org/yassine/metier/Interface/Empruntable.java
@@ -1,8 +1,22 @@
 package org.yassine.metier.Interface;
 
+import org.yassine.metier.Abstract.Utilisateur;
+import org.yassine.metier.JournalScientifique;
+import org.yassine.metier.Livre;
+import org.yassine.metier.Magazine;
+import org.yassine.metier.TheseUniversitaire;
+
 public interface Empruntable {
 
-    boolean emprunter();
-    boolean retourner();
+
+    void empruntLivre(Livre livre, Utilisateur utilisateur);
+    void empruntMagazine(Magazine magazine , Utilisateur utilisateur);
+    void empruntThese(TheseUniversitaire theseUniversitaire, Utilisateur utilisateur);
+    void empruntJOURNAL(JournalScientifique journalScientifique , Utilisateur utilisateur);
+
+    void retournLivre(Livre livre,Utilisateur utilisateur);
+    void retournMagazine(Magazine magazine ,Utilisateur utilisateur);
+    void retournThese(TheseUniversitaire theseUniversitaire,Utilisateur utilisateur);
+    void retournJournal(JournalScientifique journalScientifique ,Utilisateur utilisateur);
 
 }
