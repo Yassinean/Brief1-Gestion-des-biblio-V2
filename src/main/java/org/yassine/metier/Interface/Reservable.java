6b8c56d95f8b59c6f2a7c69ddf994dde890bda95
Yassinean <yassinedubraska@gmail.com>
2024-09-14 19:17:13 +0100

    adding reservable interface

--- src/main/java/org/yassine/metier/Interface/Reservable.java
+++ src/main/java/org/yassine/metier/Interface/Reservable.java
@@ -1,8 +1,21 @@
 package org.yassine.metier.Interface;
 
+import org.yassine.metier.Abstract.Utilisateur;
+import org.yassine.metier.JournalScientifique;
+import org.yassine.metier.Livre;
+import org.yassine.metier.Magazine;
+import org.yassine.metier.TheseUniversitaire;
+
 public interface Reservable {
 
-    boolean reserver();
-    boolean annulerReservation();
+    void reserveLivre(Livre livre, Utilisateur utilisateur);
+    void reserveMagazine(Magazine magazine, Utilisateur utilisateur);
+    void reserveThese(TheseUniversitaire theseUniversitaire, Utilisateur utilisateur);
+    void reserveJournal(JournalScientifique journalScientifique, Utilisateur utilisateur);
+
+    void annuleReserveLivre(Livre livre, Utilisateur utilisateur);
+    void annuleReserveMagazine(Magazine magazine, Utilisateur utilisateur);
+    void annuleReserveThese(TheseUniversitaire theseUniversitaire, Utilisateur utilisateur);
+    void annuleReserveJournal(JournalScientifique journalScientifique, Utilisateur utilisateur);
 
 }
