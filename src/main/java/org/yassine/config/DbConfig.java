d41b458d8fdbc1a027e23d112bc856ca0d25b422
Yassinean <yassinedubraska@gmail.com>
2024-09-23 14:29:50 +0100

    gitignore

--- src/main/java/org/yassine/config/DbConfig.java
+++ src/main/java/org/yassine/config/DbConfig.java
@@ -3,13 +3,20 @@ package org.yassine.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class DbConfig {
     private static volatile DbConfig instance;
     private final Connection connection;
-    private final String url = "jdbc:postgresql://localhost:5432/library";
-    private final String username = "postgres";
-    private final String password = "hanach";
+    private static final Dotenv dotenv = Dotenv.load();
+    String dbPort = dotenv.get("DB_PORT");
+    String dbName = dotenv.get("DB_NAME");
+    String dbUserName = dotenv.get("DB_USERNAME");
+    String dbPassword = dotenv.get("DB_PASSWORD");
+
+    private final String url = "jdbc:postgresql://localhost:" + dbPort + "/" + dbName;
+    private final String username = dbUserName;
+    private final String password = dbPassword;
 
     private DbConfig() throws SQLException {
         try {
